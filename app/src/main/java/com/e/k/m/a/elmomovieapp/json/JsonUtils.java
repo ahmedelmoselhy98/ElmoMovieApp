cb889e7a42cc4c508f0bbd73adcac263e2ed6ead
ahmed elmoselhy <ahmedelmoselhy98@gmail.com>
2018-03-03 22:21:35 +0200

    Initial commit

---
+++ app/src/main/java/com/e/k/m/a/elmomovieapp/json/JsonUtils.java
@@ -0,0 +1,8 @@
+package com.e.k.m.a.elmomovieapp.json;
+
+/**
+ * Created by ahmedelmoselhy on 2/18/2018.
+ */
+
+public class JsonUtils {
+}
---
+++ app/src/main/java/com/e/k/m/a/elmomovieapp/movie/MovieModel.java
@@ -0,0 +1,11 @@
+package com.e.k.m.a.elmomovieapp.movie;
+
+/**
+ * Created by ahmedelmoselhy on 2/18/2018.
+ */
+
+public class MovieModel {
+
+
+
+}
