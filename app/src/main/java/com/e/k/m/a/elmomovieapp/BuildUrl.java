0ac1f140bac0ab79ff1d0f070829c2b3d20224ff
ahmed elmoselhy <ahmedelmoselhy98@gmail.com>
2018-03-10 21:15:06 +0200

    #commit_3_stage1_2

--- app/src/main/java/com/e/k/m/a/elmomovieapp/BuildUrl.java
+++ app/src/main/java/com/e/k/m/a/elmomovieapp/BuildUrl.java
@@ -10,8 +10,8 @@ public class BuildUrl {
     private static final String POPULAR = "popular";
     private static final String TOP_RATED = "top_rated";
     private static final String API_KEY = "api_key";
-    public static final String POPULAR_MOVIE_URL = "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=41049e5d4f52d9922464f0055a20caaa";
-    public static final String TOP_RATED_MOVIE_URL = "http://api.themoviedb.org/3/discover/movie?sort_by=vote_average.desc&api_key=41049e5d4f52d9922464f0055a20caaa";
+    public static final String POPULAR_MOVIE_URL = "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=APT_KEY";
+    public static final String TOP_RATED_MOVIE_URL = "http://api.themoviedb.org/3/discover/movie?sort_by=vote_average.desc&api_key=APT_KEY";
 
 
 
