3ce1433881ad1c98d963e569d515f3249cee3be6
ahmed elmoselhy <ahmedelmoselhy98@gmail.com>
2018-03-12 09:12:42 +0200

    #commit_3_stage1_final_1

--- app/src/main/java/com/e/k/m/a/elmomovieapp/BuildUrl.java
+++ app/src/main/java/com/e/k/m/a/elmomovieapp/BuildUrl.java
@@ -6,6 +6,6 @@ package com.e.k.m.a.elmomovieapp;
 
 public class BuildUrl {
     public static final String IMAGE_BASE_URL ="http://image.tmdb.org/t/p/w185/";
-    public static final String POPULAR_MOVIE_URL = "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=API_KEY";
-    public static final String TOP_RATED_MOVIE_URL = "http://api.themoviedb.org/3/discover/movie?sort_by=vote_average.desc&api_key=API_KEY";
+    public static final String POPULAR_MOVIE_URL = "http://api.themoviedb.org/3/discover/movie/popular?api_key=API_KEY";
+    public static final String TOP_RATED_MOVIE_URL = "http://api.themoviedb.org/3/discover/movie/top_rated?api_key=API_KEY";
 }
